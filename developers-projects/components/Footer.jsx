3cdbb1c9dd250fcc720e54619b4b96533a76b88d
Nety <netanielw@gmail.com>
2022-07-19 12:45:52 +0300

    desing

--- developers-projects/components/Footer.jsx
+++ developers-projects/components/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return(
-        <div>
-            
-        </div>
+        <footer>
+            <h3>Learn, Create & Share</h3>
+            <p>Made By <a target="_blank" href='https://github.com/Nety97' rel="noopener noreferrer">Netaniel Wahnich</a></p>
+        </footer>
     )
 }
--- developers-projects/components/Header.jsx
+++ developers-projects/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header (){
     return(
         <div className="header">
             <h1>Devs projects</h1>
-            <h2>A list of Developer Projects.</h2>
+            <h2>A list of Projects made by the Community.</h2>
             <a href={`https://github.com/Nety97/devsProjects`} target='_blank' rel='noopener noreferrer'>
                 <p>Upload you project</p>
             </a>
